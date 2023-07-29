49e0e308d83a28c6178593e55eb1ac7b0c7ba633
Paul Haidukevich <paul-haik@debian.localhost>
2023-07-29 12:29:31 +0300

    Added first task

---
+++ variables/1_sum.cpp
@@ -0,0 +1,10 @@
+#include<iostream> // В этой задаче вам даны два числа, требуется найти их сумму.
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << a + b;
+}
