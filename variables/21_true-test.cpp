8718469dd6f5429b4ab59e91f5e3a75a68ec9936
Paul Haidukevich <paul-haik@debian.localhost>
2023-07-31 23:05:23 +0300

    Added bool test program

---
+++ variables/21_true-test.cpp
@@ -0,0 +1,11 @@
+#include<iostream> // В этой задаче требуется проверить, что число не равно нулю.
+
+int main(){
+    using namespace std;
+    int a;
+    bool l;
+    cin >> a;
+    l = a;
+    cout << l;
+    return 0;
+}
